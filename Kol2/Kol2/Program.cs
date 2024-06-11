9c7cbbc25c47e4132499b1d7de9158735f3abf54
s22854 <s22854@pjwstk.edu.pl>
2024-06-11 13:42:43 +0200

    map controllers

--- Kol2/Kol2/Program.cs
+++ Kol2/Kol2/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
 builder.Services.AddDbContext<MyDbContext>(o => 
     o.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
@@ -21,7 +22,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-
+app.MapControllers();
 
 app.Run();
 
